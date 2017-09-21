d71577dc40f7594325b938b795a9e1e2cb858def
hulei <hulei@nonobank.com>
2017-09-21 11:55:10 +0800

    页面首次载人 login

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,10 +1,31 @@
 import React from 'react'
 
 class App extends React.Component {
+  constructor(props, context) {
+    super(props, context);
+    this.state = {
+      login: false
+    }
+  }
+
   render() {
     return (
-      <div>{this.props.children}</div>
+      <div>
+        {
+          this.state.login
+            ? this.props.children
+            : <div>正在加载...</div>
+        }
+      </div>
     )
+  }
+
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        login: true
+      })
+    }, 2000)
   }
 }
 
