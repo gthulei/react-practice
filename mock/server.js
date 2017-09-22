c14438581250f693f5406db3b5f0f9644b2080c5
hulei <hulei@nonobank.com>
2017-09-22 13:41:28 +0800

    模拟线上接口

--- mock/server.js
+++ mock/server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const app = express();
 
+let result = function (data,errorCode='0000000',errorMessage='成功') {
+  return {
+    "errorCode": errorCode,
+    "errorMessage": errorMessage,
+    "data": data
+  }
+}
+
 // 首页 —— 广告（超值特惠）
 const homeAdData = require('./home/ad.js');
 
 app.get('/api/homead', (req, res) => {
   console.log('首页 —— 广告（超值特惠）')
-  res.json(homeAdData);
+  res.json(result(homeAdData));
 });
 
 // 首页 —— 推荐列表（猜你喜欢）
@@ -21,7 +29,7 @@ app.get('/api/homelist/:city/:page', (req, res) => {
 
   console.log('当前城市：' + paramsCity)
   console.log('当前页数：' + paramsPage)
-  res.json(homeListData);
+  res.json(result(homeListData));
 });
 
 // 搜索结果页 - 搜索结果 - 三个参数
@@ -40,7 +48,7 @@ app.get('/api/search/:page/:city/:category/:keyword', (req, res) => {
   console.log('当前城市：' + paramsCity)
   console.log('当前类别：' + paramsCategory)
   console.log('关键字：' + paramsKeyword)
-  res.json(searchListData)
+  res.json(result(searchListData))
 })
 // 搜索结果页 - 搜索结果 - 两个参数
 app.get('/api/search/:page/:city/:category', (req, res) => {
@@ -56,7 +64,7 @@ app.get('/api/search/:page/:city/:category', (req, res) => {
   console.log('当前城市：' + paramsCity)
   console.log('当前类别：' + paramsCategory)
 
-  res.json(searchListData);
+  res.json(result(searchListData));
 })
 
 // 详情页 - 商户信息
@@ -68,7 +76,7 @@ app.get('/api/detail/info/:id', (req, res) => {
   const id = params.id
 
   console.log('商户id: ' + id)
-  res.json(detailInfo)
+  res.json(result(detailInfo))
 })
 // 详情页 - 用户评论
 const detailComment = require('./detail/comment.js')
@@ -81,7 +89,7 @@ app.get('/api/detail/comment/:page/:id', (req, res) => {
 
   console.log('商户id: ' + id)
   console.log('当前页数: ' + page)
-  res.json(detailComment)
+  res.json(result(detailComment))
 })
 
 // 订单列表
@@ -91,37 +99,26 @@ app.get('/api/orderlist/:username', (req, res) => {
   const params = this.params
   const username = params.username
   console.log('用户名：' + username)
-  res.json(orderList)
+  res.json(result(orderList))
 })
 
 
 // 购买
 app.post('/api/order', (req, res) => {
   console.log('buy')
-  res.json({
-    errno: 0,
-    msg: 'buy ok',
-  })
+  res.json(result(null))
 });
 
 // 登录
 app.post('/api/login', (req, res) => {
   console.log('login')
-  res.json({
-    errno: 0,
-    msg: 'loing ok',
-    token: 'aaaaaaaaaaaa'
-  })
+  res.json(result('登录成功'))
 });
 
 // 获取短信验证码
 app.post('/api/sms', (req, res) => {
   console.log('获取短信验证码')
-  res.json({
-    errno: 0,
-    msg: 'ok',
-    code: 'md5(123456)'
-  })
+  res.json(result('获取短信验证码'))
 });
 
 
@@ -129,10 +126,7 @@ app.post('/api/sms', (req, res) => {
 app.post('/api/submitComment', (req, res) => {
   console.log('提交评论')
   // 获取参数
-  res.json({
-    errno: 0,
-    msg: 'ok'
-  })
+  res.json(result('提交评论'))
 
 })
 
