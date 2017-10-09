dcafe95e21a9c07c5ad0b191c99a789f9f530166
hulei <hulei@nonobank.com>
2017-10-09 12:09:08 +0800

    fastclick

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import { render } from 'react-dom'
+import fastclick from 'fastclick'
 import { hashHistory } from 'react-router'
 import { Provider } from 'react-redux'
 import configureStore from './redux/store/configureStore'
 import RouteMap from './router/routeMap'
 
 import 'static/css/index.scss'
+
+fastclick.attach(document.body)
+
 // 创建 Redux 的 store 对象
 const store = configureStore()
 
